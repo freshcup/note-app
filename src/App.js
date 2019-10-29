06c04c55dfb7c0053f80a2eb6c06683fab9b55cb
Mike Tracz <miketracz@hotmail.com>
2019-10-29 15:22:55 +0000

    RESTful API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -19,16 +20,22 @@ class App extends Component {
     });
   }
 
-  render() {
-    const { showNote } = this.state;
+  getNotes = () => {
+    axios.get('https://note-api-freshcup.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+}
 
-    return (
-      <div className="App">
-        <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List /> }
-      </div>
-    );
-  }
+  render() {
+  const { showNote } = this.state;
+
+  return (
+    <div className="App">
+      <Nav toggleNote={this.toggleNote} showNote={showNote} />
+      { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
+    </div>
+  );
+}
 }
 
 export default App;
