3a7a027ad6f063697cdbf20cb1d4a633e1a7f492
Mike Tracz <miketracz@hotmail.com>
2019-11-01 16:28:30 +0000

    error handling

--- src/components/Note.js
+++ src/components/Note.js
@@ -16,10 +16,9 @@ class Note extends React.Component {
     const formData = {
       name: this.name.value
     };
-    console.log(formData);
     this.props.submitTag(formData, this.props.note.id)
     this.props.closeTagForm();
-  }
+}
 
   renderTagForm(note) {
     if (note.id !== undefined) {
@@ -50,14 +49,33 @@ class Note extends React.Component {
     }
   }
 
-  
+  renderTags(note) {
+    if (note.tags) {
+      return note.tags.map((tag, index) =>
+        <div
+          className="tag"
+          key={index}
+          onClick={(e) => this.props.deleteTag(note.id, tag.id)}
+        >
+          <span className="delete">
+            <i className="material-icons">delete</i>
+          </span>
+          {tag.name}
+        </div>
+      );
+    }
+  }
 
   render () {
-    const { note } = this.props;
+    const { note, closeTagForm } = this.props;
 
     return (
       <div className="note-container">
-         <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
+          <form
+          className="note-form"
+          onSubmit={(e) => this.onSubmit(e)} 
+          onClick={() => closeTagForm()}
+        >
           <input
             className="note-title-input"
             type="text"
@@ -77,6 +95,9 @@ class Note extends React.Component {
           <div className="tag-button-container">
             {this.renderTagForm(note)}
           </div>
+          <div className="tag-list-container">
+            {this.renderTags(note)}
+          </div>        
         </div>
       </div>
     );
